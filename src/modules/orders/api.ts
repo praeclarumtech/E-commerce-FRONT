8e85a859394b987a3be58582ad882b09d6acab17
ALIN <alin.kp025@gmail.com>
2026-02-02 15:17:37 +0530

    add master-module

--- src/modules/orders/api.ts
+++
@@ -1,6 +0,0 @@
-import api from "../../shared/api";
-import { PaginationParams } from "../../shared/types";
-
-export function getOrder({ params }: { params: PaginationParams }) {
-    return api.get('/city', { params });
-}
